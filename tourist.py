406a8437da3d82d53c12e1a4b65a3c0e582f0c67
Jisobe <91648032+Jisobe@users.noreply.github.com>
2024-06-04 16:55:37 -0500

    added test objects

---
+++ tourist.py
@@ -0,0 +1,2 @@
+destionations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brasil", "Cairo, Egypt"];
+test_traveler = ["Erin Wilkes", "Shanghai, China", ["historical sites", "art"]]
